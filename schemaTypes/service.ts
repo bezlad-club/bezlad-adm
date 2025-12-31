66d19e870dd76b192672b711bd09e2189d382803
Vsevolod Volkov <73965070+StLyn4@users.noreply.github.com>
2025-12-31 14:07:34 +0100

    Remove `paymentUrl` field from service schema

--- schemaTypes/service.ts
+++ schemaTypes/service.ts
@@ -38,12 +38,6 @@ export const service = defineType({
       type: 'number',
       validation: (Rule) => Rule.required().min(0),
     }),
-    defineField({
-      name: 'paymentUrl',
-      title: 'Посилання на оплату',
-      type: 'url',
-      validation: (Rule) => Rule.required().uri({allowRelative: false}),
-    }),
   ],
   preview: {
     select: {
